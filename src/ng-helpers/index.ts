015d86ce499bea4191b91938ba1a8bdf94684f1f
Gianluca Cagnin <gianluca.cagnin@robyone.net>
2017-12-15 16:29:26 +0100

    dsa

--- src/ng-helpers/index.ts
+++ src/ng-helpers/index.ts
@@ -2,5 +2,5 @@
 import * as ht from "./http-error-to-modal/index";
 export {moduleExists} from "./module-exists";
 export {ngUtilsService} from "./service";
-export var HttpErrorToModal=ht;
+export {init} from "./init";
 
